1fa2d2833cc49030dd9b475a9cbb2eef622ba20f
mskang21 <minsungk0212@gmail.com>
2023-08-16 21:56:26 -0400

    edited

--- js/Cursor.js
+++ js/Cursor.js
@@ -1,17 +1,2 @@
-const cursor = document.querySelector(".cursor");
-document.addEventListener("mousemove", (e) => {
-   let leftPosition = e.pageX;
-   let topPosition = e.pageY;
 
-   cursor.style.left = leftPosition + "px";
-   cursor.style.top = topPosition + "px";
-})
-// $('.cursor').each(function(i, obj) {
-//   document.addEventListener("mousemove", (e) => {
-//     let leftPosition = e.pageX;
-//     let topPosition = e.pageY;
-  
-//     cursor.style.left = leftPosition + "px";
-//     cursor.style.top = topPosition + "px";
-//   })
-// })
+ 
